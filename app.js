a27475cca59d5244ee5247c5633b3c4979198173
Karan <102166167+karanadam@users.noreply.github.com>
2023-01-06 13:01:55 +0530

    Update app.js
    Connected sliders.

--- app.js
+++ app.js
@@ -1,11 +1,43 @@
+let button = document.getElementById("main-button"); // Main Button
 
-let button = document.getElementById('main-button'); // Main Button
+document.getElementById("button-text").addEventListener("input", function () {
+  let buttonTextInput = document.getElementById("button-text").value; // Get input value
+  button.textContent = buttonTextInput;
+});
 
+function updateTextInput(val, id) {
+  document.getElementById(id).value = val;
+}
 
-document.getElementById('button-text').addEventListener('input',function(){
-    let buttonTextInput = document.getElementById('button-text').value; // Get input value
-    button.textContent = buttonTextInput;
-})
+document.getElementById("font-size").addEventListener("change", function () {
+  let fontSize = document.getElementById("font-size").value; // Get input value
+  button.style.fontSize = fontSize;
+});
 
+document.getElementById("font-weight").addEventListener("change", function () {
+  let fontWeight = document.getElementById("font-weight").value; // Get input value
+  button.style.fontWeight = fontWeight;
+});
 
-// button.style.fontSize = whatever value;
+document
+  .getElementById("radius-slider")
+  .addEventListener("change", function () {
+    let borderRadius = document.getElementById("radius-slider").value;
+    button.style.borderRadius = borderRadius;
+  });
+
+document.getElementById("slider-x").addEventListener("change", function () {
+  let xSlider = document.getElementById("slider-x").value;
+  button.style.paddingInline = xSlider;
+});
+
+document.getElementById("slider-y").addEventListener("change", function () {
+  let ySlider = document.getElementById("slider-y").value;
+  button.style.paddingBlock = ySlider;
+});
+
+function changeBgColour() {
+  button.style.color = doumcent.getElementById("button-color").value;
+}
+
+// button.style.backgroundColor = color;
