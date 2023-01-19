54e6996f82a9becf7cf12b7da932c354d92eb836
sid-js <sidhsreejil@gmail.com>
2023-01-19 21:57:38 +0530

    Added copy buttons

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-let button = document.getElementById("main-button"); // Main Button
+let button = document.getElementById("main-button"); // Main 
+let buttonTextInput = "Get Started"
 
 document.getElementById("button-text").addEventListener("input", function () {
-  let buttonTextInput = document.getElementById("button-text").value; // Get input value
+  buttonTextInput = document.getElementById("button-text").value; // Get input value
   button.textContent = buttonTextInput;
 });
 
@@ -47,3 +48,43 @@ document.getElementById("button-color-text").addEventListener("input",function()
   button.style.color = buttonTextColor;
 
 })
+
+
+const getCSS = () => {
+  var targetDOMElement = document.querySelector("#main-button");
+var targetObjsStyles = window.getComputedStyle(targetDOMElement);
+console.log('ORIGINAL SET (' + Object.keys(targetObjsStyles).length + ' rules):',targetObjsStyles)
+
+var tempCopyOfTarget = document.createElement(targetDOMElement.tagName);
+document.body.insertAdjacentElement('afterBegin', tempCopyOfTarget);
+
+var basicElementsCSS = window.getComputedStyle(tempCopyOfTarget);
+console.log('BASELINE (DUMMY OBJECT) SET (' + Object.keys(basicElementsCSS).length + ' rules):',basicElementsCSS)
+
+var cleanSetOfStyles = {};
+Object.entries(targetObjsStyles).forEach(p=>{
+    if(basicElementsCSS[p[0]] !== p[1]){ 
+        cleanSetOfStyles[p[0]] = p[1];
+    }
+
+});
+console.log(cleanSetOfStyles)
+const finalCSS = `.main-button {
+  background-color: ${cleanSetOfStyles.backgroundColor};
+  color: ${cleanSetOfStyles.color};
+  font: ${cleanSetOfStyles.font};
+  padding: ${cleanSetOfStyles.padding};
+  border-radius: ${cleanSetOfStyles.borderRadius};
+}`
+
+navigator.clipboard.writeText(finalCSS);
+tempCopyOfTarget.remove()
+}
+
+const getHTML = () => {
+  const finalHTML = `
+  <button class="main-btn" id="main-button">
+  ${buttonTextInput}
+</button>`
+navigator.clipboard.writeText(finalHTML);
+}
